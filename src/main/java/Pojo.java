f87f5c6c896e0ee5e2dded8338309849a2541f25
ranhui <17725763053@163.com>
2019-08-10 20:36:54 +0800

    A2 用户第一次修改代码

--- src/main/java/Pojo.java
+++ src/main/java/Pojo.java
@@ -7,4 +7,16 @@
 public class Pojo {
     private String name;
 
+    /**
+     * A2 用户第一次修改代码
+     */
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
